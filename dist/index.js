71593be2123c298b1da6cd2cd19aee5f5eb955ee
YMakwanaIH <yogesh.makwana@invitationhomes.com>
2022-09-15 14:07:09 +0530

    first commit

--- dist/index.js
+++ dist/index.js
@@ -3558,8 +3558,6 @@ const main = async () => {
       repo,
       issue_number: pr_number,
       body: `
-      smit dajriiiiiiiiiiiiii
-      1:37
         Pull Request #${pr_number} has been updated with Smit: \n
         - ${diffData.changes} changes \n
         - ${diffData.additions} additions \n
