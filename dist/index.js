3478aa514696bd24d10aa192f861d28c3e1db7a1
YMakwanaIH <yogesh.makwana@invitationhomes.com>
2022-09-15 13:29:44 +0530

    first commit

--- dist/index.js
+++ dist/index.js
@@ -3562,7 +3562,6 @@ const main = async () => {
         - ${diffData.changes} changes \n
         - ${diffData.additions} additions \n
         - ${diffData.deletions} deletions \n
-        test
       `
     });
 
--- index.js
+++ index.js
@@ -105,7 +105,7 @@ const main = async () => {
       repo,
       issue_number: pr_number,
       body: `
-        Pull Request #${pr_number} has been hi with: \n
+        Pull Request #${pr_number} has been updated with: \n
         - ${diffData.changes} changes \n
         - ${diffData.additions} additions \n
         - ${diffData.deletions} deletions \n
